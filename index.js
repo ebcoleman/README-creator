8116cfb4ec64e574b89bc0cd7f96dc9ca583b3e3
ebcoleman <140854597+ebcoleman@users.noreply.github.com>
2024-01-28 10:04:19 -0500

    corrected the questions const, still needs work but want to push the changes so the repo is up to date.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const path = require('path');
 const generateMarkdown = require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -15,6 +17,53 @@ const questions = [
     type: 'input',
     name: 'description',
     message: 'Please provide a short description of your project.',
+  },
+  {
+    // this needs to be done differently than the rest, not sure what as of right now
+    type: 'input',
+    name: 'tableOfContents',
+    message: '',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What are the steps required to install your project?',
+  },
+  {
+    type: 'input',
+    name: 'screenshot',
+    message: 'Please provide the filepath for the screenshot to your project. Example: assets/images/screenshot.png',
+  },
+  {
+    type: 'input',
+    name: 'credits',
+    message: 'Who/what did you use and need to credit in your project?',
+  },
+  {
+// THIS PART NEEDS REVIEW. DONT KNOW ENOUGH ABOUT LICENSES; this shouldn't be a question. Maybe a list to choose from? that would change the type from input to something else
+    type: 'input',
+    name: 'license',
+    message: 'Choose your license:',
+  },
+  {
+      type: 'input',
+      name: 'badges',
+      message: '',
+  },
+  {
+      type: 'input',
+      name: 'features',
+      message: '',
+  },
+  {
+      type: 'input',
+      name: 'contribute',
+      message: '',
+  },
+  {
+      type: 'input',
+      name: 'test',
+      message: '',
   },
 ];
 
@@ -34,69 +83,10 @@ inquirer
   })
   .catch((err) => console.error(err));
 
-// inquirer
-//   .prompt([
-    // {
-    //   type: 'input',
-    //   name: 'title',
-    //   message: 'What is the title of your project?',
-    // },
-    // {
-    //   type: 'input',
-    //   name: 'description',
-    //   message: 'Please provide a short description of your project.',
-    // },
-//     {
-//         // this needs to be done differently than the rest, not sure what as of right now
-//         type: 'input',
-//         name: 'tableOfContents',
-//         message: '',
-//     },
-//     {
-//       type: 'input',
-//       name: 'installation',
-//       message: 'What are the steps required to install your project?',
-//     },
-//     {
-//       type: 'input',
-//       name: 'screenshot',
-//       message: 'Please provide the filepath for the screenshot to your project. Example: assets/images/screenshot.png',
-//     },
-//     {
-//       type: 'input',
-//       name: 'credits',
-//       message: 'Who/what did you use and need to credit in your project?',
-//     },
-//     {
-// // THIS PART NEEDS REVIEW. DONT KNOW ENOUGH ABOUT LICENSES; this shouldn't be a question. Maybe a list to choose from? that would change the type from input to something else
-//       type: 'input',
-//       name: 'license',
-//       message: 'Choose your license:',
-//     },
-//     {
-//         type: 'input',
-//         name: 'badges',
-//         message: '',
-//     },
-//     {
-//         type: 'input',
-//         name: 'features',
-//         message: '',
-//     },
-//     {
-//         type: 'input',
-//         name: 'contribute',
-//         message: '',
-//     },
-//     {
-//         type: 'input',
-//         name: 'test',
-//         message: '',
-//     },
-//   ])
-//   .then((answers) => {
-//     const readmePageContent = generateMarkdown(answers);
-//   }
+
+    
+    
+
 
 // TODO: Create a function to write README file
 function writeToFile(README, data) {}
